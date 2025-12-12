542571c8d89b3d4183bcd39c68671a84b791dcba
Kareem Shehata <kareem@shehata.ca>
2025-12-12 12:15:24 +0800

    Attempt at bridging a network interface from the client to the relay used by the DKLS library.

--- rust/src/types.rs
+++ rust/src/types.rs
@@ -1,9 +1,17 @@
 use std::sync::Arc;
+use futures::Future;
+use futures::{sink, Sink, Stream};
+use core::pin::Pin;
 use rand::{Rng, SeedableRng};
 use rand_chacha::ChaCha20Rng;
 use k256::elliptic_curve::group::GroupEncoding;
 
 use crate::error::NetworkError;
+
+
+/*****************************************************************************
+ * Wrappers for basic types.
+ *****************************************************************************/
 
 #[repr(transparent)]
 #[derive(Clone, Copy, Debug, uniffi::Object)]
@@ -41,6 +49,11 @@ impl Keyshare {
     }
 }
 
+
+/*****************************************************************************
+ * Placeholders for keys.
+ *****************************************************************************/
+
 #[derive(uniffi::Object)]
 pub struct NodeSecretKey { }
 
@@ -64,6 +77,11 @@ impl NodeVerifyingKey {
         self.0.clone()
     }
 }
+
+/*****************************************************************************
+ * Network interface provides an interface for clients to implement network
+ * functionality.
+ *****************************************************************************/
 
 #[uniffi::export(with_foreign)]
 #[async_trait::async_trait]
@@ -96,4 +114,100 @@ impl NetworkInterfaceTester {
         }
         Ok(())
     }
+}
+
+
+/*****************************************************************************
+ * Bridge async network interface for UniFFI to the Source + Sink relay that
+ * SL DKLS23 uses.
+ *****************************************************************************/
+
+type BoxedStream<T> = Pin<Box<dyn Stream<Item = T> + Send>>;
+type BoxedSink<T, E> = Pin<Box<dyn Sink<T, Error = E> + Send>>;
+
+pub struct Duplex<T, E> {
+    rx: BoxedStream<T>,
+    tx: BoxedSink<T, E>,
+}
+
+impl<T, E> Duplex<T, E> {
+    pub fn new(rx: BoxedStream<T>, tx: BoxedSink<T, E>) -> Self {
+        Self { rx, tx }
+    }
+}
+
+impl<T, E> Stream for Duplex<T, E> {
+    type Item = T;
+
+    fn poll_next(
+        mut self: Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<Option<Self::Item>> {
+        self.rx.as_mut().poll_next(cx)
+    }
+}
+
+impl<T, E> Sink<T> for Duplex<T, E> {
+    type Error = E;
+
+    fn poll_ready(self: std::pin::Pin<&mut Self>, cx: &mut std::task::Context<'_>) -> std::task::Poll<Result<(), Self::Error>> {
+        unsafe { self.map_unchecked_mut(|s| &mut s.tx) }.poll_ready(cx)
+    }
+
+    fn start_send(self: std::pin::Pin<&mut Self>, item: T) -> Result<(), Self::Error> {
+        unsafe { self.map_unchecked_mut(|s| &mut s.tx) }.start_send(item)
+    }
+
+    fn poll_flush(self: std::pin::Pin<&mut Self>, cx: &mut std::task::Context<'_>) -> std::task::Poll<Result<(), Self::Error>> {
+        unsafe { self.map_unchecked_mut(|s| &mut s.tx) }.poll_flush(cx)
+    }
+
+    fn poll_close(self: std::pin::Pin<&mut Self>, cx: &mut std::task::Context<'_>) -> std::task::Poll<Result<(), Self::Error>> {
+        unsafe { self.map_unchecked_mut(|s| &mut s.tx) }.poll_close(cx)
+    }
+}
+
+// Have to explicitly mark as meeting the trait.
+impl sl_dkls23::Relay for Duplex<Vec<u8>, sl_dkls23::MessageSendError> {    
+}
+
+// Helper to write things to the network interface needed for unfold.
+fn network_write_fn(
+    interface: Arc<dyn NetworkInterface>, 
+    item: Vec<u8>
+) -> impl Future<Output = Result<Arc<dyn NetworkInterface>, sl_dkls23::MessageSendError>> {
+    
+    // We clone the Arc to move it into the async block, 
+    // ensuring the Arc's reference count is managed correctly.
+    let interface_clone = interface.clone(); 
+    
+    async move {
+        let res = interface_clone.send(item).await;
+            match res {
+            Ok(_) => Ok(interface_clone),
+            Err(_) => Err(sl_dkls23::MessageSendError),
+        }
+    }
+}
+
+// Create an adapter that meets the SL DKLS23 Relay trait based on an ARC to the
+// NetworkInterface from the client.
+// NB: there's no way to detect errors from the receive state - it's assumed that
+// clients will somehow pick this up!!
+pub fn create_network_relay(interface: Arc<dyn NetworkInterface>) -> impl sl_dkls23::Relay {
+    let interface_clone = interface.clone();
+    let rx = async_stream::stream! {
+        loop {
+            let received = interface_clone.receive().await;
+            match received {
+                Ok(data) => yield data,
+                Err(_) => yield Vec::new(), // Silently return an empty array on error
+            }
+        }
+    };
+    let tx = sink::unfold(
+        interface,
+        network_write_fn
+    );
+    Duplex::new(Box::pin(rx), Box::pin(tx))
 }
